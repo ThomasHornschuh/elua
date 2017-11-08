1dfed2e25721fd0359f9929e92f15992ea05cf9c
Thomas Hornschuh <thomas@hornschuh-family.de>
2017-11-08 19:55:04 +0100

    Changes to support bonfire_soc_io

--- src/platform/bonfire/uart.c
+++ src/platform/bonfire/uart.c
@@ -1,23 +1,20 @@
-/* Test UART */
-// sample_clk = (f_clk / (baudrate * 16)) - 1
-// (32.000.000 / (115200*16))-1 = 16,36 ...
-// UART Base: 0x20000000
+
 //    |--------------------|--------------------------------------------|
 //--! | Address            | Description                                |
 //--! |--------------------|--------------------------------------------|
-//--! | 0x00               | Transmit register (write-only)             |
-//--! | 0x04               | Receive register (read-only)               |
-//--! | 0x08               | Status register (read-only)                |
-//--! | 0x0c               | Sample clock divisor register (read/write) |
-//--! | 0x10               | Interrupt enable register (read/write)     |
-// !  | 0x14               | Revision Code                              |
+//--! | 0x00               | Transmit register(W)/  Receive register(R) |
+//--! | 0x04               | Status(R)) and control(W) register         |
 //--! |--------------------|--------------------------------------------|
 
 //--! The status register contains the following bits:
-//--! - Bit 0: receive buffer empty
-//--! - Bit 1: transmit buffer empty
-//--! - Bit 2: receive buffer full
-//--! - Bit 3: transmit buffer full
+//--! - Bit 0: UART RX Ready bit. Reads as 1 when there's received data in FIFO, 0 otherwise.
+//--! - Bit 1: UART TX Ready bit. Reads as 1 when there's space in TX FIFO for transmission, 0 otherwise.
+
+// Control register:
+// Bit [15:0] - UARTPRES UART prescaler (16 bits)   (f_clk / (baudrate * 16)) - 1
+// Bit 16 - UARTEN UARTEN bit controls whether UART is enabled or not
+
+
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -36,10 +33,19 @@
 
 #define ENABLE_SEND_DELAY 0
 
+#define UART_TX 0
+#define UART_RECV 0
+#define UART_STATUS 1
+#define UART_CONTROL 1
+//#define UART_INTE 0x10
+//#define UART_REVISION 0x14
 
-volatile uint8_t *uartadr=(uint8_t *)UART_BASE;
+//#define ENABLE_SEND_DELAY 1
 
-volatile uint8_t *gpioadr=(uint8_t *)GPIO_BASE;
+
+volatile uint32_t *uartadr=(uint32_t *)UART_BASE;
+volatile uint32_t *gpioadr=(uint32_t *)GPIO_BASE;
+
 
 
 void wait(long nWait)
@@ -56,24 +62,23 @@ void uart_writechar(char c)
 {
 
 #ifdef  ENABLE_SEND_DELAY
-   wait(1000);     
+   wait(1000);
 #endif
-  while (uartadr[UART_STATUS] & 0x08); // Wait while transmit buffer full
-  uartadr[UART_TX]=(uint8_t)c;
+  while (!(uartadr[UART_STATUS] & 0x2)); // Wait while transmit buffer full
+  uartadr[UART_TX]=(uint32_t)c;
 
 }
-
-
 
 char uart_readchar()
 {
-  while (uartadr[UART_STATUS] & 0x01); // Wait while receive buffer empty
-  return uartadr[UART_RECV];
+  while (!(uartadr[UART_STATUS] & 0x01)); // Wait while receive buffer empty
+  return (char)uartadr[UART_RECV];
 }
 
 
+
 // adpated version for elua:
-// if timeout==0 and data from the UART is available the data is returned, otherwise -1 is returned 
+// if timeout==0 and data from the UART is available the data is returned, otherwise -1 is returned
 // if timeout != 0 then it waits until data read from the UART becomes available
 int uart_wait_receive(long timeout)
 {
@@ -81,12 +86,12 @@ uint8_t status;
 
   do {
     status=uartadr[UART_STATUS];
-    if ((status & 0x01)==0) { // receive buffer not empty?
+    if  (status & 0x01) { // receive buffer not empty?
       return uartadr[UART_RECV];
     }
-     
+
   }while(timeout);
-  return -1;    
+  return -1;
 }
 
 
@@ -104,40 +109,42 @@ void uart_write_str(char *p)
 void uart_write_console(char *p)
 {
    while (*p) {
-    if (*p=='\n') uart_writechar('\r');   
+    if (*p=='\n') uart_writechar('\r');
     uart_writechar(*p);
     p++;
-  } 
-    
+  }
+
 }
 
 
-
+static uint16_t l_divisor=0;
 void _setDivisor(uint32_t divisor){
-    
-   uartadr[UART_DIVISOR]=divisor; // Set Baudrate divisor  
+
+   l_divisor = divisor;
+   uartadr[UART_CONTROL]= 0x010000L | (uint16_t)divisor; // Set Baudrate divisor and enable port
 }
+
 
 void setDivisor(uint32_t divisor)
 {
-    _setDivisor(divisor);   
+    _setDivisor(divisor);
     wait(1000000);
 }
 
 uint32_t getDivisor()
 {
-  return uartadr[UART_DIVISOR];    
+  return l_divisor;
 }
 
 void uart_setBaudRate(int baudrate) {
 // sample_clk = (f_clk / (baudrate * 16)) - 1
-// (96.000.000 / (115200*16))-1 = 51,08    
-    
-   setDivisor(SYSCLK / (baudrate*16) -1); 
+// (96.000.000 / (115200*16))-1 = 51,08
+
+   setDivisor(SYSCLK / (baudrate*16) -1);
 }
 
 uint8_t getUartRevision()
 {
-   return uartadr[UART_REVISION];   
-     
+   return 0x0ff; // not supported with ZPUINO UART yet
+
 }
